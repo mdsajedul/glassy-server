f3885aa93120407567d097f3e8e6ce03064d9b76
mdsajedul <sajedulislms@gmail.com>
2021-11-16 19:59:03 +0600

    delete api

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run(){
             const orders = await ordersCollection.find(query).toArray();
             res.send(orders);
         })
+
+        //order delete api 
+      app.delete('/orders/:id' ,async(req , res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const result = await ordersCollection.deleteOne(query);
+        res.send(result);
+      })
         
 
     }
